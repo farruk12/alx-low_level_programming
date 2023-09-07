a39a142c2d3bc8fd132150e6a91017be55d94c77
farruk12 <farruk.omar@icloud.com>
2023-09-07 11:18:59 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - c programming is awesome
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art,");
+return 0;
+}
