896dce3604405a8e890f6c8a192b3bf7954a80be
farruk12 <farruk.omar@icloud.com>
2023-09-07 13:11:39 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - C program that prints the size of various types of data
+ * Return: Always 0 (success)
+*/
+int main(void)
+}
+int a;
+char b;
+float c;
+long d;
+printf("Size of char: %zu byte(s)\n", sizeof(b));
+printf("Size of int: %zu byte(s)\n", sizeof(a));
+printf("Size of long: %zu byte(s)\n", sizeof(d));
+printf("Size of float: %zu byte(s)\n", sizeof(c));
+return (0);
+}
