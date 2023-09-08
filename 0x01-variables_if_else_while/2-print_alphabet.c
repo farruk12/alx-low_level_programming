d0f7387bd8dec28ff9d3c58d9571d4e599b89397
farruk12 <farruk.omar@icloud.com>
2023-09-08 14:29:15 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: 'working on 3rd question'
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char  alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+
+	return (0);
+}
