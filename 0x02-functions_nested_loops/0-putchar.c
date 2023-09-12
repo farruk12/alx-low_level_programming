8663a6348cbb872cec42323a12f625ee3547ce4b
farruk12 <farruk.omar@icloud.com>
2023-09-12 12:54:45 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
 /**
  * main - main entry
  * description: C - Functions, nested loops
@@ -6,7 +7,16 @@
 */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 
 	return (0);
 }
