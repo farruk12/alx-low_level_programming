9b759a90cc73b9dce6f89724bbe7ad57a293f421
farruk12 <farruk.omar@icloud.com>
2023-09-14 14:49:37 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,19 +8,20 @@ void print_square(int size)
 {
 	if (size <= 0)
 	{
-		_putchar('\n');
+	_putchar('\n');
 	}
 	else
 	{
-		int i, j;
+	int i, j;
 
-		for (i = 0; i <= size; i++)
-		{
-			for (j = 0; j <= size; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
+	for (i = 0; i < size; i++)
+	{
+	for (j = 0; j < size; j++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	}
 	}
 }
+
