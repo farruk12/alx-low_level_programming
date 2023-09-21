0a02da919ab9b01cc5137bafec8182a75408bbf1
farruk12 <farruk.omar@icloud.com>
2023-09-21 14:01:53 -0700

    update

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ *
+ * Return: *s
+ */
+char *rot13(char *s)
+{
+	int i;
+	int j;
+
+	char omar[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char farruk[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[1] == omar[j])
+			{
+				s[i] = farruk[i];
+				break;
+			}
+		}
+	}
+	return (s);
+}
