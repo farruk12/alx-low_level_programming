4debd7ddd7c5a1c44dc8d334c4d2774b50f31da8
farruk12 <farruk.omar@icloud.com>
2023-09-25 14:17:46 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Entry point
+ *
+ *
+ * @s: string
+ * @accept: string
+ * Return: always (success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; i++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + 1);
+			}
+		}
+	}
+
+	return ('\0');
+}
+
