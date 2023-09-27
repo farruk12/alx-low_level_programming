65f5119811ec68bc8cee396b50f92a551a32b017
farruk12 <farruk.omar@icloud.com>
2023-09-27 09:40:32 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen_recursion - Entry point
+ * @s: input
+ * Return: Always success
+ */
+int _strlen_recursion(char *s)
+{	
+	return strlen(s);
+}
