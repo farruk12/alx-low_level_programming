046643d10972dd1a75dcb8d7b15d4b6cbbc0ecd6
farruk12 <farruk.omar@icloud.com>
2023-09-27 13:10:20 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,32 +1,40 @@
 #include "main.h"
 
 /**
- * is_prime_number - Entry point
+ * is_prime_helper - Entry point
  * @n: input
- * Return: always succes
+ * @divisor: input
+ * Return: always success
 */
 
-int is_prime_helper(int n, int divisor) 
+int is_prime_helper(int n, int divisor)
 {
-	if (n <= 1) {
-	return 0;
+	if (n <= 1)
+	{
+	return (0);
 	}
-	if (divisor == 1) {
-	return 1;
+	if (divisor == 1)
+	{
+	return (1);
 	}
-	if (n % divisor == 0) {
-	return 0;
+	if (n % divisor == 0)
+	{
+	return (0);
 	}
-    
-	return is_prime_helper(n, divisor - 1);
+	return (is_prime_helper(n, divisor - 1));
 }
 
-int is_prime_number(int n) 
-{
-    if (n <= 1) 
-	{
-	return 0;
-	}
+/**
+ * is_prime_number - Entry point
+ * @n: input
+ * Return: always success
+ */
 
-	return is_prime_helper(n, n - 1);
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+	return (0);
+	}
+	return (is_prime_helper(n, n - 1));
 }
