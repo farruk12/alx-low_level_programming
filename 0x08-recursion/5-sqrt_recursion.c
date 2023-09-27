9eb19c5a5a3dc3ba304a51972fab7097748113b5
farruk12 <farruk.omar@icloud.com>
2023-09-27 11:36:11 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Entry point
+ * @n: input
+ * Return: always succes
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+
+	return (Myfunction(n, 1));
+}
+
+int Myfunction(int n, int guess)
+{
+	if (guess * guess == n)
+	{
+		return (guess);
+	}
+	if (guess * guess > n)
+	{
+		return (-1);
+	}
+	return (Myfunction(n, guess + 1));
+}
