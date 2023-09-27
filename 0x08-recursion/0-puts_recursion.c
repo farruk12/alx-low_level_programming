23365242dda3660508f6e47868cf7622d6dc453d
farruk12 <farruk.omar@icloud.com>
2023-09-27 10:26:56 -0700

    updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts_recursion - main entry
@@ -10,9 +9,9 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-	printf("\n");
-	return;
+		_putchar('\n');
+		return;
 	}
-	printf("%c", *s);
-	_puts_recursion(s + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 }
