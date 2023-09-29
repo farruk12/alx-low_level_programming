e4e16aaf3cd960779272465bb67ef8b5fdce563a
farruk12 <farruk.omar@icloud.com>
2023-09-29 13:22:48 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc:  integer
+ * @argv:  string
+ * Return: always success
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc == 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
+		return (0);
+	}
+	else
+	{
+		printf("error\n");
+		return (1);
+	}
+}
+
+
