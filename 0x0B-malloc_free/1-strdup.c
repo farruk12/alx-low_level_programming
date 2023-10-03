a4ce81c48f8cf7c3bf29781590c8a3d2c748eedf
farruk12 <farruk.omar@icloud.com>
2023-10-03 08:05:13 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - entry point
+ * @str: string
+ * Return: always (success)
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int i;
+	int str_len = 0;
+
+	while (str[str_len] != '\0')
+	{
+		str_len++;
+	}
+
+	p = malloc(str_len + 1);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		p[i] = str[i];
+	}
+
+	p[i] = '\0';
+
+	return (p);
+}
