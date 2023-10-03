0002293336068caf314f686a95f82195092e76c1
farruk12 <farruk.omar@icloud.com>
2023-10-03 04:27:13 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,11 @@ char *create_array(unsigned int size, char c)
 {
 	char *ptr;
 	unsigned int i;
-
+	
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	ptr = malloc(size);
 	for (i = 0; i < size; i++)
 	{
