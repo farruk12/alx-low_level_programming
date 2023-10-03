4cbb7d1925a51e68c0c16e77979968e199f3e7cd
farruk12 <farruk.omar@icloud.com>
2023-10-03 15:05:25 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * argstostr - main entry
+ * @ac: integer
+ * @av: string
+ * Return: always (success)
+ */
+char *argstostr(int ac, char **av)
+{
+	int length;
+	int i, j;
+	char *combination;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			length++;
+		}
+
+		length++;
+	}
+
+	combination = (char *)malloc((length + 1) * sizeof(char));
+	if (combination == NULL)
+	{
+		return (NULL);
+	}
+
+	length = 0;
+	for (i = 0; i <ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			combination[length++] = av[i][j];
+		}
+		combination[length++] = '\n';
+	}
+
+	combination[length] = '\0';
+	return (combination);
+}
