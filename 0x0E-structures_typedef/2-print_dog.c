07f5fe778519cd2d39ae942b4748a8fdfb243572
farruk12 <farruk.omar@icloud.com>
2023-10-09 13:18:58 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,6 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-
 	(d->name == NULL) ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
 	if (d->age >= 0)
 		printf("Age: %f\n", d->age);
