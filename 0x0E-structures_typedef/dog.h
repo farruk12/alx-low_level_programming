5aa2cd17655105a6b0319e4e61a43dbe54e52c75
farruk12 <farruk.omar@icloud.com>
2023-10-09 11:05:15 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - entry point
+ * @name: char
+ * @age: float
+ * @owner: char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
