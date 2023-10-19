700042c55890402b47bff83936c533e486876b69
farruk12 <farruk.omar@icloud.com>
2023-10-19 13:55:57 -0700

    task 2update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,22 +12,22 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (str == NULL)
 		return (NULL);
-
 	while (str[len] != '\0')
 		len++;
-
 	new_node = malloc(sizeof(list_t));
+
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->len = len;
 	new_node->str = strdup(str);
 
-	if (new_node->str != NULL)
+	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
+
 	new_node->next = *head;
 	*head = new_node;
 
