02584b3b01fa0c143d0e18a349e6e999d47a972d
farruk12 <farruk.omar@icloud.com>
2023-10-19 08:31:04 -0700

    task 0 Singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,8 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)");
 		else
-			printf("%s\n", h ->str);
+			printf("[%d] %s\n", h->len, h ->str);
+
 		h = h->next;
 		count++;
 	}
