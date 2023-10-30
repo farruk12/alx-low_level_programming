4b9243e89799495cfcc0ef638431ac8c59f35e61
farruk12 <farruk.omar@icloud.com>
2023-10-30 10:54:09 -0700

    task 2 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | 0_APPEND, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND, 0600);
 	if (fd == -1)
 		return (-1);
 
