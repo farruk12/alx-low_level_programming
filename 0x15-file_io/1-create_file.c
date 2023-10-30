15ad958a5fc5af477ea3fc4193b68e7232daeb5f
farruk12 <farruk.omar@icloud.com>
2023-10-30 10:08:37 -0700

    task 1 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file.
+ * @filename: const char pointer
+ * @text_content: char pointer
+ * Return: always success
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_write;
+	ssize_t length;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		length = strlen(text_content);
+		bytes_write = write(fd, text_content, length);
+		if (bytes_write == -1 || bytes_write != length)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-
+#include <string.h>
+int create_file(const char *filename, char *text_content);
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
